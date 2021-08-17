f63fe73dffa5849d047a2ce15be5d3cf7ca68d79
Henry <henry-pan@users.noreply.github.com>
2021-08-16 21:54:23 -0700

    Add timer logic

---
+++ src/app.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+export default function App() {
+  const [hours, setHours] = useState(0);
+  const [mins, setMins] = useState(0);
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const timeLeft = 640 - (((Date.now() / 1000) / 60) % 640);
+      setHours(Math.trunc(timeLeft / 60));
+      setMins(Math.trunc(timeLeft % 60));
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [hours, mins]);
+
+
+  return (
+    <div className="content">
+      <header>
+        <h1>Hespori Timer</h1>
+      </header>
+      <section>
+        <h2>{hours} hours, {mins} mins</h2>
+      </section>
+    </div>
+  );
+}
